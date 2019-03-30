b82791abaee6d719f9c8c24e10a10f97bfaa8563
JimTheCactus <jimthecactus@gmail.com>
2019-03-30 23:58:12 +0000

    Fixed attack start/stop scripts
    They had additional logging enabled that was likely writing to
    directories that they didn't have access to. Removed that
    to bring it in line with start.sh's behavior

--- tests/attack/start_rogue.sh
+++ tests/attack/start_rogue.sh
@@ -4,5 +4,5 @@ echo "Killing any existing rogue AS"
 ./stop_rogue.sh
 
 echo "Starting rogue AS"
-sudo python run.py --node R4 --cmd "zebra -f /etc/quagga/zebra.conf -d -i /tmp/zebra-R4.pid"
-sudo python run.py --node R4 --cmd "bgpd -f /etc/quagga/bgpd.conf -d -i /tmp/bgpd-R4.pid"
+sudo python run.py --node R4 --cmd "zebra -f /etc/quagga/zebra.conf -d"
+sudo python run.py --node R4 --cmd "bgpd -f /etc/quagga/bgpd.conf -d"
