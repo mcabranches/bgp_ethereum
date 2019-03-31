78739aebe58675968aa67c6abbfa72fb5851e7f2
JimTheCactus <jimthecactus@gmail.com>
2019-03-31 02:33:25 +0000

    Adjusted IP addresses to match zebra files.

--- tests/attack/setup_network.py
+++ tests/attack/setup_network.py
@@ -35,19 +35,23 @@ sc = net.addSwitch('s7')
 
 info('*** Adding docker containers\n')
 
-R1 = net.addDocker('R1', ip="11.0.0.1/8", dimage="r1-quagga")
-R2 = net.addDocker('R2', ip="12.0.0.1/8", dimage="r2-quagga")
-R3 = net.addDocker('R3', ip="13.0.0.1/8", dimage="r3-quagga")
+R1 = net.addDocker('R1', ip="11.0.1.254/24", dimage="r1-quagga")
+R2 = net.addDocker('R2', ip="12.0.1.254/24", dimage="r2-quagga")
+R3 = net.addDocker('R3', ip="13.0.1.254/24", dimage="r3-quagga")
 # Attacker node
-R4 = net.addDocker('R4', ip="13.0.0.1/8", dimage="r4-quagga")
+R4 = net.addDocker('R4', ip="13.0.1.254/24", dimage="r4-quagga")
 
 # add end host code here
-WS = net.addDocker('WS', ip="13.0.1.1/8", dimage="ws-quagga")
-AWS = net.addDocker('AWS', ip="13.0.1.1/8", dimage="ws-quagga")
-C1 = net.addDocker('C1', ip="11.0.0.2/8", dimage="quaggabase")
+WS = net.addDocker('WS', ip="13.0.1.1/24", dimage="ws-quagga")
+AWS = net.addDocker('AWS', ip="13.0.1.1/24", dimage="ws-quagga")
+C1 = net.addDocker('C1', ip="11.0.1.1/24", dimage="quaggabase")
 
 
 info("Configuring IP addresses of the local connections\n")
+
+# Attack link
+net.addLink(R1,sc,params1={"ip":"9.0.4.1/24"})
+net.addLink(R4,sc,params1={"ip":"9.0.4.2/24"})
 
 # Link routers to each other
 net.addLink(R1,sa,params1={"ip":"9.0.0.1/24"})
@@ -55,20 +59,16 @@ net.addLink(R2,sa,params1={"ip":"9.0.0.2/24"})
 net.addLink(R2,sb,params1={"ip":"9.0.1.1/24"})
 net.addLink(R3,sb,params1={"ip":"9.0.1.2/24"})
 
-# Attack link
-net.addLink(R1,sc,params1={"ip":"9.0.4.1/24"})
-net.addLink(R4,sc,params1={"ip":"9.0.4.2/24"})
-
 # Link routers to their associated AS switches
-net.addLink(R1,s1,params1={"ip":"11.0.0.1/8"})
-net.addLink(R2,s2,params1={"ip":"12.0.0.1/8"})
-net.addLink(R3,s3,params1={"ip":"13.0.0.1/8"})
-net.addLink(R4,s4,params1={"ip":"13.0.0.1/8"})
+net.addLink(R1,s1,params1={"ip":"11.0.1.254/24"})
+net.addLink(R2,s2,params1={"ip":"12.0.1.254/24"})
+net.addLink(R3,s3,params1={"ip":"13.0.1.254/24"})
+net.addLink(R4,s4,params1={"ip":"13.0.1.254/24"})
 
 # Link hosts to their AS switches
-net.addLink(C1,s1,params1={"ip":"11.0.0.2/8"})
-net.addLink(WS,s3,params1={"ip":"13.0.1.1/8"})
-net.addLink(AWS,s4,params1={"ip":"13.0.1.1/8"})
+net.addLink(C1,s1,params1={"ip":"11.0.1.1/24"})
+net.addLink(WS,s3,params1={"ip":"13.0.1.1/24"})
+net.addLink(AWS,s4,params1={"ip":"13.0.1.1/24"})
 
 R1.cmd('/start.sh &')
 R2.cmd('/start.sh &')
@@ -82,11 +82,11 @@ R3.cmd('/start.sh &')
 R4.cmd('ifconfig')
 
 WS.cmd('route del default')
-WS.cmd('route add default gw 13.0.0.1')
+WS.cmd('route add default gw 13.0.1.254')
 AWS.cmd('route del default')
-AWS.cmd('route add default gw 13.0.0.1')
+AWS.cmd('route add default gw 13.0.1.254')
 C1.cmd('route del default')
-C1.cmd('route add default gw 11.0.0.1')
+C1.cmd('route add default gw 11.0.1.254')
 
 WS.cmd('/webserver.py &')
 AWS.cmd("/webserver.py --text 'Attacking Webserver' &")
